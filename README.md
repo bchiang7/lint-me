1bcb17870afcb71871e1dcc09b9cdb00e0c8609c
bchiang7 <brittany.chiang@gmail.com>
2019-03-20 17:27:20 -0400

    I am a bash script noob

---
+++ README.md
@@ -0,0 +1,57 @@
+# Lint Me
+
+> A script to install Upstatement linting configs ([ESLint](https://www.npmjs.com/package/@upstatement/eslint-config) & [Prettier](https://www.npmjs.com/package/@upstatement/prettier-config)) to your project
+
+## Installation
+
+1. Clone this repo
+
+2. In your terminal config (i.e. `.zshrc`, `.bash_rc`, etc.) add an alias for running the install script
+
+   ```sh
+   alias lintme="sh ~/<insert-path-to-this-repo>/install.sh"
+   ```
+
+3. Now you should be able to run `lintme` at the root of any project to automatically install Upstatement's ESLint & Prettier linting configs
+
+## Usage
+
+At the root of any project, simply run
+
+```sh
+lintme <type>
+```
+
+### Default
+
+```sh
+lintme
+```
+
+### Four Spaces
+
+```sh
+lintme four
+```
+
+### React
+
+```sh
+lintme react
+```
+
+### Vue
+
+```sh
+lintme vue
+```
+
+You should end up with the following files at the root of your project
+
+- `.editorconfig`
+- `.eslintrc`
+- `.gitignore`
+- `install.sh`
+- `package-lock.json`
+- `package.json`
+- `prettier.config.js`
---
+++ install.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+
+function npmInit {
+  if [ -e ./package.json ]; then
+    echo '{
+  "name": "@upstatement/project-name",
+  "version": "0.0.0",
+  "description": "",
+  "keywords": [],
+  "author": "",
+  "license": "",
+  "main": "",
+  "scripts": {},
+  "dependencies": {},
+  "devDependencies": {}
+}' > package.json
+
+  else
+    echo 'package.json already exists'
+  fi
+}
+
+function gitIgnore {
+  echo 'node_modules
+
+.DS_Store
+
+.env
+.env.local
+.env.development
+.env.test
+.env.production
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+
+.vscode/
+' > .gitignore
+}
+
+function addHooks {
+  read -p "Do you want to use pre-commit hooks? (y/n) " choice
+  case "$choice" in
+    y|Y )
+      echo "Installing husky, pretty-quick, and lint-staged packages..."
+
+      eval "npm install -D husky pretty-quick lint-staged"
+
+      INSERT_HERE=$(( $(wc -l < package.json) - 1 ))
+
+      # add pre-commit hook configs to package.json
+      eval "head -n $INSERT_HERE package.json > temp.txt"
+
+      eval "sed '$INSERT_HERE s/$/,/' temp.txt > temp2.txt"
+
+      echo '  "husky": {
+    "hooks": {
+      "pre-commit": "pretty-quick --staged && lint-staged"
+    }
+  },
+  "lint-staged": {
+    "*.js": [
+      "eslint --fix",
+      "git add"
+    ]
+  }
+}' >> temp2.txt
+
+
+      eval "mv temp2.txt package.json && rm temp.txt"
+      ;;
+    n|N )
+      echo ''
+      ;;
+    * )
+  esac
+}
+
+SPACING=2
+
+function scaffold {
+  if [ -e ./package-lock.json ]; then
+    eval "rm package-lock.json"
+  fi
+
+  eval "rm -rf node_modules"
+
+  # create files
+  eval "touch .editorconfig prettier.config.js .eslintrc .gitignore"
+
+  # init package.json
+  npmInit
+
+  # add things to gitignore
+  gitIgnore
+
+
+  if [ -z "$1" ]; then
+    eval "npm install -D @upstatement/eslint-config @upstatement/prettier-config eslint babel-eslint prettier eslint-config-prettier"
+
+    echo '{
+  "extends": "@upstatement",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "env": {
+    "browser": true,
+    "node": true
+  }
+}' > .eslintrc
+
+  elif [ $1 = four ]; then
+
+    eval "npm install -D @upstatement/eslint-config @upstatement/prettier-config eslint babel-eslint prettier eslint-config-prettier"
+
+    echo '{
+  "extends": "@upstatement/eslint-config/four-spaces",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "env": {
+    "browser": true,
+    "node": true
+  }
+}' > .eslintrc
+
+    SPACING=4
+
+  elif [ $1 = react ]; then
+
+    eval "npm install -D @upstatement/eslint-config @upstatement/prettier-config eslint babel-eslint prettier eslint-config-prettier eslint-plugin-react eslint-plugin-jsx-a11y"
+
+    echo '{
+  "extends": "@upstatement/eslint-config/react"
+}' > .eslintrc
+
+  elif [ $1 = vue ]; then
+
+    eval "npm install -D @upstatement/eslint-config @upstatement/prettier-config eslint babel-eslint prettier eslint-config-prettier  eslint-plugin-vue vue-eslint-parser"
+
+    echo '{
+  "extends": "@upstatement/eslint-config/vue"
+}' > .eslintrc
+
+  else
+    echo 'Uh oh something went wrong'
+  fi
+
+  echo "root = true
+
+  [*]
+  charset = utf-8
+  indent_style = space
+  indent_size = $SPACING
+  end_of_line = lf
+  insert_final_newline = true
+  trim_trailing_whitespace = true" > .editorconfig
+
+  if [ $SPACING = 4 ]; then
+    echo "module.exports = require('@upstatement/prettier-config/four-spaces');" > prettier.config.js
+  else
+    echo "module.exports = require('@upstatement/prettier-config');" > prettier.config.js
+  fi
+
+  addHooks
+}
+
+# Default config
+if [ -z "$1" ]; then
+  echo 'Setting up default linting configs...'
+  scaffold
+
+# Four spaces config
+elif [ $1 = four ]; then
+  echo "Setting up four spaces linting config..."
+  scaffold four
+
+# React config
+elif [ $1 = react ]; then
+  echo "Setting up react linting config..."
+  scaffold react
+
+# Vue Config
+elif [ $1 = vue ]; then
+  echo "Setting up vue linting config..."
+  scaffold vue
+
+else
+  echo 'Please specify a valid config, such as `react` or `vue`'
+fi
